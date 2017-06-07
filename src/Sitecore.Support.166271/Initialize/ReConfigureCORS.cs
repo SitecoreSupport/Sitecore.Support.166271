cf9d7bf3304fc404a63a77978aa3d6fd1d267465
Alexey Lunev <alu@sitecore.net>
2017-06-07 18:47:41 +0300

    Initial Commit

---
+++ src/Sitecore.Support.166271/Initialize/ReConfigureCORS.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Web.Routing;
+
+
+namespace Sitecore.Support.Initialize
+{
+  public class ReConfigureCORS
+  {
+
+    public virtual void Process(PipelineArgs args)
+    {
+      /*var route = System.Web.Routing.RouteTable.Routes.MapHttpRoute(
+        name: "FxmBeaconCustom",
+        routeTemplate: "sitecore/api/ssc/Beacon/Service/beacon/{action}",
+        defaults: new { controller = "CustomBeacon" } 
+        );*/
+      GlobalConfiguration.Configuration.EnableCors();
+    }
+  }
+}
